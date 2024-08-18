55904067c349b8008fcb60e8eedcbd91b11845ed
Lucas Emanoel Rodrigues da Silva <lucws2002@gmail.com>
2024-08-18 16:38:17 -0400

    feat: 🎸 add theme toggle in dashboard main menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | dashboard.manager' />
-      <Toaster richColors />
-      <RouterProvider router={router}></RouterProvider>
+      <ThemeProvider defaultTheme='dark' storageKey='dashboard-theme'>
+        <Helmet titleTemplate='%s | dashboard.manager' />
+        <Toaster richColors />
+        <RouterProvider router={router}></RouterProvider>
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
