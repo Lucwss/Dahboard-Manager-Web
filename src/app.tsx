2324ffedf2b7287791fa1e0b88f6fee78fd4f756
Lucas Emanoel Rodrigues da Silva <lucws2002@gmail.com>
2024-08-19 15:34:41 -0400

    feat: 🎸 add api client configuration

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,8 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
 
@@ -12,7 +14,9 @@ export function App() {
       <ThemeProvider defaultTheme='dark' storageKey='dashboard-theme'>
         <Helmet titleTemplate='%s | dashboard.manager' />
         <Toaster richColors />
-        <RouterProvider router={router}></RouterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
